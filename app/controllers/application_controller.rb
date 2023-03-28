823c5d08043d6ba58755399401c3130f5b6d22ad
Sean Abbas <sean.abbas33@gmail.com>
2023-03-28 11:46:01 -0400

    start frontend auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-    before_action :snake_case_params
+    protect_from_forgery with: :exception
+    before_action :snake_case_params, :attach_authenticity_token
 
     def current_user
       @current_user ||= User.find_by(session_token: session[:session_token])
@@ -38,8 +39,12 @@ class ApplicationController < ActionController::API
 
 
     private
-    
+
     def snake_case_params
       params.deep_transform_keys!(&:underscore)
+    end
+
+    def attach_authenticity_token
+      headers['X-CSRF-Token'] = masked_authenticity_token(session)
     end
 end
